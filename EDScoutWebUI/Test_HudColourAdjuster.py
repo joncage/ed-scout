24126ab0d21f6df2d53916bf495f35ff136bb9d8
Jon Cage <Jon@digital-explosion.co.uk>
2020-08-14 00:35:18 +0100

    * Added options to convert #FFFFFF html colour representations. * Next step is to split out the .css, parse it to replace values and check results.

--- EDScoutWebUI/Test_HudColourAdjuster.py
+++ EDScoutWebUI/Test_HudColourAdjuster.py
@@ -8,3 +8,46 @@ def test_can_read_values_correctly():
     assert matrix_values['MatrixRed'] == [1, 0, 0]
     assert matrix_values['MatrixGreen'] == [0, 1, 0]
     assert matrix_values['MatrixBlue'] == [0, 0, 1]
+
+
+def test_colour_conversion_with_identity_does_no_change_colours():
+    colour_matrix = {
+        'MatrixRed': [1, 0, 0],
+        'MatrixGreen': [0, 1, 0],
+        'MatrixBlue': [0, 0, 1],
+    }
+
+    original_colour = [0.5, 0.5, 0.5]
+
+    new_colour = adjust_colours(original_colour, colour_matrix)
+
+    assert new_colour == [0.5, 0.5, 0.5]
+
+
+
+def test_colour_conversion_make_red_green():
+    colour_matrix = {
+        'MatrixRed': [0.0, 1.0, 0.0],
+        'MatrixGreen': [0.0, 1.0, 0.0],
+        'MatrixBlue': [0.0, 0.0, 1.0],
+    }
+
+    original_colour = [0.5, 0.5, 0.5]
+
+    new_colour = adjust_colours(original_colour, colour_matrix)
+
+    assert new_colour == [0.0, 1.0, 0.5]
+
+def test_colour_conversion_make_red_green_as_hex():
+    colour_matrix = {
+        'MatrixRed': [0.0, 1.0, 0.0],
+        'MatrixGreen': [0.0, 1.0, 0.0],
+        'MatrixBlue': [0.0, 0.0, 1.0],
+    }
+
+    original_colour = "#808080"
+
+
+    new_colour = hex_colour_shift(original_colour, colour_matrix)
+
+    assert new_colour == "#00FF80"
