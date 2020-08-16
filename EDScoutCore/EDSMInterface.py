e2e6bec188ea46d1bb3b7a3469fca02a7be1f1da
Jon Cage <Jon@digital-explosion.co.uk>
2020-08-16 20:53:36 +0100

    * Adding tests to get more info.

--- EDScoutCore/EDSMInterface.py
+++ EDScoutCore/EDSMInterface.py
@@ -1,5 +1,7 @@
 import requests
 
+# See https://www.edsm.net/en_GB/api-v1
+
 
 def get_systems(system_name, radius):
     requestData = {
@@ -11,6 +13,19 @@ def get_systems(system_name, radius):
 
     if data.status_code != 200:
         raise ("request returned bad response code %d" % (data.status_code))
+    return data.json()
+
+
+def get_bodies(system_name):
+    requestData = {
+        "systemName": system_name
+    }
+
+    data = requests.get("https://www.edsm.net/api-system-v1/bodies", requestData)
+
+    if data.status_code != 200:
+        raise ("request returned bad response code %d" % (data.status_code))
+
     return data.json()
 
 
@@ -34,9 +49,8 @@ def search_systems(system_name, radius):
         print(data)
 
 
-def lookup_system():
+def lookup_system(system_name):
     # {'StarSystem': 'Wregoe JS-K d8-38', 'SystemAddress': 1316231366987, 'StarPos': [380.375, 215.84375, -299.46875], 'StarClass': 'F'}
-    system_name = 'Wregoe JS-K d8-38'
     system_value = get_system_estimated_value(system_name)
     print(system_value)
     print('nearby :')
