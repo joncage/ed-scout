c5c4ed1ee0d6ab21ac43d0d2d06f3c3a207d88a3
Jon Cage <Jon@digital-explosion.co.uk>
2020-08-21 21:52:03 +0100

    * Started working towards populating values.

--- EDScoutCore/BodyAppraiser.py
+++ EDScoutCore/BodyAppraiser.py
@@ -1,4 +1,11 @@
 
+# Uses calculations from here: https://forums.frontier.co.uk/threads/exploration-value-formulae.232000/
+
+
+def appraise_star(star_info):
+    # (k + (m * k / 66.25))
+    return 0
 
 def appraise_body(body_info):
+    # k + (k * m[SIZE=1]0.2[/SIZE] * q)
     return "???"
