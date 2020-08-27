bdb1714815281f03b4062f8309ed2d35fe945d3d
Jon Cage <Jon@digital-explosion.co.uk>
2020-08-27 08:37:12 +0100

    * Fixed Flake8 whinging

--- EDScoutCore/OutputRecorder.py
+++ EDScoutCore/OutputRecorder.py
@@ -1,27 +1,27 @@
 from .ZmqWrappers import Receiver
+import json
+from logging import log
 
 
 class OutputRecorder():
 
     def __init__(self, port):
+        zmq_port_test = None
+        self.r = Receiver(port=zmq_port_test)
 
-    zmq_port_test=None
-    r = Receiver(port=zmq_port_test)
-
-
-
-while True:
-    message = r.receive().decode('ascii')
-    try:
-        log.debug("Received:   '" + message + "'")
-        content = dict(data=json.loads(message))
-        log.debug("Forwarding: '" + str(content) + "'")
-        socketio.emit('log', content, broadcast=True)
-    except Exception as pass_on_failure:
-        log.exception(pass_on_failure)
+    def run(self):
+        while True:
+            message = self.r.receive().decode('ascii')
+            try:
+                log.debug("Received:   '" + message + "'")
+                content = dict(data=json.loads(message))
+                log.debug("Forwarding: '" + str(content) + "'")
+                # socketio.emit('log', content, broadcast=True)
+            except Exception as pass_on_failure:
+                log.exception(pass_on_failure)
 
 
-JournalInterface.JournalWatcher._on_journal_change
-    JournalChangeProcessor.process_journal_change
-        JournalChangeProcessor.binary_file_data_to_lines -> yields entries
-            EDScout.forward_journal_change
+# JournalInterface.JournalWatcher._on_journal_change
+#     JournalChangeProcessor.process_journal_change
+#         JournalChangeProcessor.binary_file_data_to_lines -> yields entries
+#               EDScout.forward_journal_change
