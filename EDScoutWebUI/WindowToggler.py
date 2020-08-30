e69668284ea7f74b324fab8f5c7ccabd27d10d1b
Jon Cage <Jon@digital-explosion.co.uk>
2020-08-30 23:29:19 +0100

    * Refactored the toggling a bit. * Hooked in the window toggling.

--- EDScoutWebUI/WindowToggler.py
+++ EDScoutWebUI/WindowToggler.py
@@ -3,63 +3,59 @@ import win32gui as wgui
 from pynput import keyboard
 
 
-def get_scout_handle():
-    return wgui.FindWindow(None, "ED Scout v1.4.0")
+class ScoutToggler:
 
+    def __init__(self):
+        # The key combination to check
+        self.COMBINATIONS = [
+            {keyboard.Key.cmd, keyboard.KeyCode(char='z')},
+            {keyboard.Key.cmd, keyboard.KeyCode(char='Z')}
+        ]
 
-def get_elite_handle():
-    return wgui.FindWindow(None, "Elite - Dangerous (CLIENT)")
+        # The currently active modifiers
+        self.current = set()
 
+        self.scout_toggled = False
 
-def hide_scout():
-    scout_handle = get_scout_handle()
-    elite_handle = get_elite_handle()
-    wgui.SetWindowPos(scout_handle, elite_handle, 0,0,0,0, wcon.SWP_NOMOVE | wcon.SWP_NOSIZE | wcon.SWP_NOACTIVATE)
+        self.listener = keyboard.Listener(
+            on_press=self.on_press,
+            on_release=self.on_release)
+        self.listener.start()
 
+    @staticmethod
+    def get_scout_handle():
+        return wgui.FindWindow(None, "ED Scout v1.4.0")
 
-def show_scout():
-    scout_handle = get_scout_handle()
-    wgui.SetWindowPos(scout_handle, wcon.HWND_TOPMOST, 0,0,0,0, wcon.SWP_NOMOVE | wcon.SWP_NOSIZE | wcon.SWP_NOACTIVATE)
-    wgui.SetWindowPos(scout_handle, wcon.HWND_NOTOPMOST, 0,0,0,0, wcon.SWP_NOMOVE | wcon.SWP_NOSIZE | wcon.SWP_NOACTIVATE)
+    @staticmethod
+    def get_elite_handle():
+        return wgui.FindWindow(None, "Elite - Dangerous (CLIENT)")
 
-#############
+    @staticmethod
+    def hide_scout():
+        scout_handle = ScoutToggler.get_scout_handle()
+        elite_handle = ScoutToggler.get_elite_handle()
+        wgui.SetWindowPos(scout_handle, elite_handle, 0,0,0,0, wcon.SWP_NOMOVE | wcon.SWP_NOSIZE | wcon.SWP_NOACTIVATE)
 
-# The key combination to check
-COMBINATIONS = [
-    {keyboard.Key.cmd, keyboard.KeyCode(char='z')},
-    {keyboard.Key.cmd, keyboard.KeyCode(char='Z')}
-]
+    @staticmethod
+    def show_scout():
+        scout_handle = ScoutToggler.get_scout_handle()
+        wgui.SetWindowPos(scout_handle, wcon.HWND_TOPMOST, 0,0,0,0, wcon.SWP_NOMOVE | wcon.SWP_NOSIZE | wcon.SWP_NOACTIVATE)
+        wgui.SetWindowPos(scout_handle, wcon.HWND_NOTOPMOST, 0,0,0,0, wcon.SWP_NOMOVE | wcon.SWP_NOSIZE | wcon.SWP_NOACTIVATE)
 
-# The currently active modifiers
-current = set()
+    def toggle_scout_visibility(self):
+        self.scout_toggled
+        if self.scout_toggled:
+            ScoutToggler.hide_scout()
+        else:
+            ScoutToggler.show_scout()
+        self.scout_toggled = not self.scout_toggled
 
-scout_toggled = False
+    def on_press(self, key):
+        if any([key in COMBO for COMBO in self.COMBINATIONS]):
+            self.current.add(key)
+            if any(all(k in self.current for k in COMBO) for COMBO in self.COMBINATIONS):
+                self.toggle_scout_visibility()
 
-def execute():
-    global scout_toggled
-    if scout_toggled:
-        hide_scout()
-    else:
-        show_scout()
-    scout_toggled = not scout_toggled
-
-def on_press(key):
-    if any([key in COMBO for COMBO in COMBINATIONS]):
-        current.add(key)
-        if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS):
-            execute()
-
-def on_release(key):
-    if any([key in COMBO for COMBO in COMBINATIONS]):
-        current.remove(key)
-
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
-
-
-if __name__ == "__main__":
-    #show_scout()
-    #import time
-    #time.sleep(1)
-    #hide_scout()
-    pass
+    def on_release(self, key):
+        if any([key in COMBO for COMBO in self.COMBINATIONS]):
+            self.current.remove(key)
